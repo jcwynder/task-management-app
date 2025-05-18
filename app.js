cd8643145ca5747c5ed3870fc3cf076bb0a4440d
Jermal Wynder <jermalwynder@gmail.com>
2025-05-18 12:22:28 -0400

    initial stage of javascript creation (variable initialization/declaration)

---
+++ app.js
@@ -0,0 +1,14 @@
+// Empty array initialization/declaration to store objects
+let tasks = [];
+
+/*
+Initialization/declaration for variables using DOM method to retrieve reference to DOM elements
+This section doesn't include variables using DOM method to create new elements
+*/
+const taskName = document.getElementById("taskName");
+const category = document.getElementById("category");
+const deadline = document.getElementById("deadline");
+const status = document.getElementById("status");
+const tbody = document.getElementById("taskContent");
+const filter = document.getElementById("combinedFilter");
+const combinedFilterElement = document.getElementById("combinedFilter");
