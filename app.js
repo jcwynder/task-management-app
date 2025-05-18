0c804130f585912121456a6a22aff21ad27c329f
Jermal Wynder <jermalwynder@gmail.com>
2025-05-18 13:23:20 -0400

    add task data functionality created (functionality not implemented until task data is rendered into project)

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 // Empty array initialization/declaration to store objects
 let tasks = [];
 
-/*
-Initialization/declaration for variables using DOM method to retrieve reference to DOM elements
-This section doesn't include variables using DOM method to create new elements
-*/
-const taskName = document.getElementById("taskName");
-const category = document.getElementById("category");
-const deadline = document.getElementById("deadline");
-const status = document.getElementById("status");
 const tbody = document.getElementById("taskContent");
 const filter = document.getElementById("combinedFilter");
 const combinedFilterElement = document.getElementById("combinedFilter");
@@ -24,5 +16,31 @@ window.onload = function () {
   if (savedTasks) {
     tasks = JSON.parse(savedTasks);
   }
-  displayTasks();
+  displayTasks(); // Dynamically updates task data display
 };
+
+// Function to add task data
+function addTask() {
+  // Initialization/declaration for variables using DOM method to retrieve reference to DOM elements
+  const taskName = document.getElementById("taskName").value.trim();
+  const category = document.getElementById("category").value.trim();
+  const deadline = document.getElementById("deadline").value;
+  const status = document.getElementById("status").value;
+
+  // if statement used to alert user to input data using all input fields if any field(s) is left blank upon submission
+  if (!taskName || !category || !deadline) {
+    alert("Please fill all fields.");
+    return;
+  }
+
+  // Adds a new task object (using the properties listed) to the tasks array
+  tasks.push({ taskName, category, deadline, status });
+  saveTasks(); // Stores updated task data in user's local storage
+  displayTasks(); // Dynamically updates task data display
+
+  // Clears input fields after user submits data from input fields
+  document.getElementById("taskName").value = "";
+  document.getElementById("category").value = "";
+  document.getElementById("deadline").value = "";
+  document.getElementById("status").value = "In Progress";
+}
